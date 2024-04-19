b7002ac81e6aad0bf5f4e25f5958cb6be9867b98
Edgar Santana <edgarfranciscos@gmail.com>
2024-04-19 04:42:12 -0600

    primer endpoint seccion 1 done

--- app/scraper.py
+++ app/scraper.py
@@ -1,11 +1,43 @@
-import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+import requests
+import json
+import os
 
-def scrape_stars_list():
-    url = 'https://iau.org/public/themes/naming_stars/'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    stars_list = []
-    for row in soup.select('.ent-name'):
-        stars_list.append(row.text.strip())
-    return stars_list
+class Scrape:
+    def __init__(self):
+        self.url = 'https://iau.org/public/themes/naming_stars/'
+        self.json_data = []
+
+    def scrape_stars_list(self):
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            stars_list = []
+            table = soup.find('table', class_='table table-striped sortable')
+            if table:
+                columns = [column.text.strip() for column in table.find('tr').find_all('th')]
+                for row in table.find_all('tr')[1:]:
+                    cells = row.find_all('td')
+                    star_info = {}
+                    for index, cell in enumerate(cells):
+                        star_info[columns[index]] = cell.text.strip()
+                    stars_list.append(star_info)
+                self.json_data = json.dumps(stars_list, indent=2)
+                return {"status": "success", "data": self.json_data}
+            else:
+                return {"status": "error", "message": "No se encontró la tabla en la página."}
+        else:
+            return {"status": "error", "message": f"No se pudo acceder a la página. Código de estado: {response.status_code}"}
+        
+        
+    def scrape_stars_local_save(self):
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        filename = f"data_{timestamp}.json"
+        folder_path = "data"
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        file_path = os.path.join(folder_path, filename)
+        data = json.loads(self.json_data)
+        with open(file_path, "w") as json_file:
+            json.dump(data, json_file, indent=4)
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
-from app.scraper import scrape_stars_list
+from app.scraper import Scrape
+import json
 
 app = FastAPI()
 
 @app.get("/")
-async def index():
-    return {"message": "Hola mundo"}
+def index():
+    steps= {
+        "Paso 1": "Para iniciar el Scraping ingrese a la URL http://127.0.0.1:8000/stars",
+        "Paso 2": "Para modificar un dato, proporcione una consulta POST a la URL http://127.0.0.1:8000/star_mod/<IAU Name> y en formanto JSON los datos a Modificar",
+        "Paso 3": "Para almacenar la informacion en Azure Blob Storage ingrese a la URL http://127.0.0.1:8000/stars_save"
+    }
+        
+    
+    return {f"Hola Mundo": "Este es un sistema desarrollado como ejercicio para 3B por el puesto de Backend Developer", "Instrucciones": steps}
 
-@app.get("/stars")
-async def get_stars():
-    stars_list = scrape_stars_list()
-    return {"stars": stars_list}
+@app.get("/stars", response_model=dict)
+def get_stars():
+    new_scrape = Scrape()
+    stars_list = new_scrape.scrape_stars_list()
+    if stars_list["status"] == "success":
+        new_scrape.scrape_stars_local_save()
+        return {"status": "success", "data": json.loads(stars_list["data"])}
+    else:
+        return {"status": "error", "message": stars_list["message"]}
